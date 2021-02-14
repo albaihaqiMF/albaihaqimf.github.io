f366a25c1bccf704cc16612f93fc7cc0c24dddde
albaihaqiMF <49091021+albaihaqiMF@users.noreply.github.com>
2021-02-14 13:13:05 +0700

    Add files via upload

---
+++ nadyabirthday/js/script.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+    $("#preloading").delay(2000).fadeOut('slow');
+    $('.ready').click(function(){
+        alert('Siaapp yaa?');
+        $('#myModal').fadeOut('slow');
+        $('#content').fadeIn('slow');
+    })
+});
+
+var cond = true;
+var myAudio = document.getElementById('myAudio');
+var audioBtn = document.getElementById('audioBtn');
+// myAudio.play();
+
+function mySong(){
+    cond ? audioBtn.classList.replace('fa-pause', 'fa-play') : audioBtn.classList.replace('fa-play', 'fa-pause');
+    cond ? myAudio.pause() : myAudio.play();
+    cond = !cond;
+}
